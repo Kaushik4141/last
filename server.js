2244afbfc3f7952ceb2d46bdb582efeea98fbeab
Kaushik4141 <kaushik0h0s@gmail.com>
2025-03-11 16:23:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // Rate limiting
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 200, // limit each IP to 100 requests per windowMs
     message: 'Too many requests, please try again later.'
 });
 
